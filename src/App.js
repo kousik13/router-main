9419d94e0401470378c5959dcf31b4cdc98f37e7
kousik13 <sendmailstome123@gamail.com>
2023-07-17 17:34:32 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import {Route, Switch} from 'react-router-dom'
+
+import Header from './components/Header'
+import About from './components/About'
+import Home from './components/Home'
+import Contact from './components/Contact'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <div className="app-container">
+    <div className="app-card">
+      <Header />
+
+      <div className="app-body">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/About" component={About} />
+          <Route exact path="/Contact" component={Contact} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </div>
+  </div>
+)
+
+export default App
